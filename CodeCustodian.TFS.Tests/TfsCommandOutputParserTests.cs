9d96178a1a6a7c20ec7d415ff5fdf2d83d922e78
Austin Thompson <purpleflux@gmail.com>
2013-04-26 06:47:52 -0500

    Added red unit tests for ParseMappedFoldersOutput

--- CodeCustodian.TFS.Tests/TfsCommandOutputParserTests.cs
+++ CodeCustodian.TFS.Tests/TfsCommandOutputParserTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CodeCustodian.TFS.Tests
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Text;
 
@@ -47,7 +48,7 @@ namespace CodeCustodian.TFS.Tests
 
             var workspaces = this.parser.ParseWorkspacesOutput(output).ToList();
 
-            Assert.IsTrue(!workspaces.Any());
+            Assert.IsFalse(workspaces.Any());
         }
 
         [TestMethod]
@@ -57,7 +58,50 @@ namespace CodeCustodian.TFS.Tests
 
             var workspaces = this.parser.ParseWorkspacesOutput(output).ToList();
 
-            Assert.IsTrue(!workspaces.Any());
+            Assert.IsFalse(workspaces.Any());
+        }
+
+        [TestMethod]
+        public void ParseMappedFoldersOutput_Should_ReturnNoFolders_When_TheInputIsNotValid()
+        {
+            var output = @"C:\folder";
+
+            var workspaces = this.parser.ParseWorkspacesOutput(output).ToList();
+
+            Assert.IsFalse(workspaces.Any());
+        }
+
+        [TestMethod]
+        public void ParseMappedFoldersOutput_Should_ReturnOneFolder_When_TheValidInputHasOneFolder()
+        {
+            const string Folder1 = @"C:\Folder";
+            var folders = new Dictionary<string, string>()
+                              {
+                                  { "Workspace1", Folder1 }
+                              };
+            var output = this.CreateMappedFoldersOutput(folders);
+
+            var workspaces = this.parser.ParseWorkspacesOutput(output).ToList();
+
+            Assert.IsTrue(workspaces.Single() == Folder1);
+        }
+
+        [TestMethod]
+        public void ParseMappedFoldersOutput_Should_ReturnAllFolders_When_TheValidInputHasMultipleFolders()
+        {
+            const string Folder1 = @"C:\Folder";
+            const string Folder2 = @"D:\Folder Name\";
+            var folders = new Dictionary<string, string>()
+                              {
+                                  { "Workspace1", Folder1 } ,
+                                  { "Workspace1", Folder2}
+                              };
+            var output = this.CreateMappedFoldersOutput(folders);
+
+            var workspaces = this.parser.ParseWorkspacesOutput(output).ToList();
+
+            Assert.IsTrue(workspaces[0] == Folder1);
+            Assert.IsTrue(workspaces[1] == Folder2);
         }
 
         private string CreateListWorkspacesOutput(params string[] workspaces)
@@ -70,6 +114,20 @@ namespace CodeCustodian.TFS.Tests
             {
                 b.Append(workspace)
                  .AppendLine(@"               DOM\user         HOSTNAME");
+            }
+
+            return b.ToString();
+        }
+
+        private string CreateMappedFoldersOutput(Dictionary<string, string> entries)
+        {
+            var b = new StringBuilder();
+            b.AppendLine(@"Workspace : MyWorkspace (DOM\user)")
+             .AppendLine("Collection: http://tfs.company.com:8080/tfs/collection");
+            foreach (var entry in entries)
+            {
+                b.AppendFormat("$/XX/{0}: {1}", entry.Key, entry.Value)
+                 .AppendLine();
             }
 
             return b.ToString();
