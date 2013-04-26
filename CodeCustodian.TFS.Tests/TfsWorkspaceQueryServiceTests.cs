df9ad8e45edc8a0f6817f8f14f08dac88987b44b
Austin Thompson <purpleflux@gmail.com>
2013-04-26 06:34:06 -0500

    Updated and added more unit tests for WorkspaceQueryService

--- CodeCustodian.TFS.Tests/TfsWorkspaceQueryServiceTests.cs
+++ CodeCustodian.TFS.Tests/TfsWorkspaceQueryServiceTests.cs
@@ -1,13 +1,11 @@
 ﻿namespace CodeCustodian.TFS.Tests
 {
     using System;
-    using System.Text;
+    using System.Linq;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     using Moq;
-
-    using System.Linq;
 
     [TestClass]
     public class TfsWorkspaceQueryServiceTests
@@ -17,41 +15,71 @@
         [TestInitialize]
         public void TestInitialize()
         {
-            this.commandFactory = new Mock<ITfsCommandFactory>();
+            var tfsCommand = new Mock<ITfsCommand>();
+            tfsCommand.Setup(x => x.Run()).Returns(new TfsCommandResult(1, string.Empty));
+            this.commandFactory = new Mock<ITfsCommandFactory>(); 
+            this.commandFactory.Setup(x => x.Create(It.IsAny<TfsCommandType>(), It.IsAny<string>())).Returns(tfsCommand.Object);
         }
 
         [TestMethod]
-        public void RetrieveAll_Should_ReturnTheWorkspace_When_TheValidInputHasOneWorkspace()
+        public void RetrieveAll_Should_ReturnTheWorkspace_When_ThereIsOneWorkspace()
         {
-            var listWorkspacesCommand = new Mock<ITfsCommand>();
-            string listWorkspacesOutput = CreateListWorkspacesOutput("workspace1");
-            listWorkspacesCommand.Setup(x => x.Run()).Returns(new TfsCommandResult(1, listWorkspacesOutput));
-            this.commandFactory.Setup(x => x.Create(TfsCommandType.ListWorkspaces, It.IsAny<string>()))
-                .Returns(listWorkspacesCommand.Object);
-            var queryService = new TfsWorkspaceQueryService(this.commandFactory.Object, new TfsCommandOutputParser());
+            const string Workspace1 = "workspace1";
+            var tfsCommandOutputParser = new Mock<ITfsCommandOutputParser>();
+            tfsCommandOutputParser.Setup(x => x.ParseWorkspacesOutput(It.IsAny<string>())).Returns(new string[] { Workspace1 });
+            tfsCommandOutputParser.Setup(x => x.ParseMappedFoldersOutput(It.IsAny<string>())).Returns(new string[0]);
+            var queryService = new TfsWorkspaceQueryService(this.commandFactory.Object, tfsCommandOutputParser.Object);
 
             var workspaces = queryService.RetrieveAll().ToList();
 
             Assert.IsTrue(workspaces.Count == 1);
-            Assert.IsTrue(workspaces[0].WorkSpaceName == "workspace1");
-            Assert.IsTrue(!workspaces[0].MappedPaths.Any());
-
+            Assert.IsTrue(workspaces[0].WorkSpaceName == Workspace1);
         }
-        
 
-        private string CreateListWorkspacesOutput(params string[] workspaces)
+        [TestMethod]
+        public void RetrieveAll_Should_ReturnAllWorkspaces_When_ThereAreMultipleWorkspaces()
         {
-            var b = new StringBuilder();
-            b.AppendLine("Collection: http://tfs.company.com:8080/tfs/collection")
-             .AppendLine("Workspace               Owner            Computer   Comment")
-             .AppendLine("----------------------- ---------------- ---------- ---------------------------");
-            foreach (var workspace in workspaces)
-            {
-                b.Append(workspace)
-                 .AppendLine(@"               DOM\user         HOSTNAME");    
-            }
-            
-            return b.ToString();
+            const string Workspace1 = "workspace1";
+            const string Workspace2 = "workspace 2";
+            var tfsCommandOutputParser = new Mock<ITfsCommandOutputParser>();
+            tfsCommandOutputParser.Setup(x => x.ParseWorkspacesOutput(It.IsAny<string>())).Returns(new string[] { Workspace1, Workspace2 });
+            tfsCommandOutputParser.Setup(x => x.ParseMappedFoldersOutput(It.IsAny<string>())).Returns(new string[0]);
+            var queryService = new TfsWorkspaceQueryService(this.commandFactory.Object, tfsCommandOutputParser.Object);
+
+            var workspaces = queryService.RetrieveAll().ToList();
+
+            Assert.IsTrue(workspaces.Count == 2);
+            Assert.IsTrue(workspaces[0].WorkSpaceName == Workspace1);
+            Assert.IsTrue(workspaces[1].WorkSpaceName == Workspace2);
+        }
+
+        [TestMethod]
+        public void RetrieveAll_Should_ReturnNoMappedFoldersForTheWorkspace_When_ThereAreNoMappedFoldersForAWorkspace()
+        {
+            const string Workspace1 = "workspace1";
+            var tfsCommandOutputParser = new Mock<ITfsCommandOutputParser>();
+            tfsCommandOutputParser.Setup(x => x.ParseWorkspacesOutput(It.IsAny<string>())).Returns(new string[] { Workspace1 });
+            tfsCommandOutputParser.Setup(x => x.ParseMappedFoldersOutput(It.IsAny<string>())).Returns(new string[0]);
+            var queryService = new TfsWorkspaceQueryService(this.commandFactory.Object, tfsCommandOutputParser.Object);
+
+            var workspaces = queryService.RetrieveAll().ToList();
+
+            Assert.IsFalse(workspaces[0].MappedPaths.Any());
+        }
+
+        [TestMethod]
+        public void RetrieveAll_Should_ReturnOneMappedFolderForTheWorkspace_When_ThereIsOneMappedFolderForAWorkspace()
+        {
+            const string Workspace1 = "workspace1";
+            const string MappedFolder1 = "mapped folder 1";
+            var tfsCommandOutputParser = new Mock<ITfsCommandOutputParser>();
+            tfsCommandOutputParser.Setup(x => x.ParseWorkspacesOutput(It.IsAny<string>())).Returns(new string[] { Workspace1 });
+            tfsCommandOutputParser.Setup(x => x.ParseMappedFoldersOutput(It.IsAny<string>())).Returns(new string[] { MappedFolder1 });
+            var queryService = new TfsWorkspaceQueryService(this.commandFactory.Object, tfsCommandOutputParser.Object);
+
+            var workspaces = queryService.RetrieveAll().ToList();
+
+            Assert.IsTrue(workspaces[0].MappedPaths.Single() == MappedFolder1);
         }
     }
 }
